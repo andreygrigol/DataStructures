1864d2a2a22dccc5307965beb7f04d9175dec2b2
AndreyRibeiro <andrey.grigoletto2@gmail.com>
2025-05-20 09:51:57 -0300

    Insertion

--- ArrayNotOrdered.py
+++ ArrayNotOrdered.py
@@ -1,17 +1,27 @@
 import numpy as np
 
+
 class ArrayNotOrditaned:
     def __init__(self, capacity):
         self.capacity = capacity
         self.last_position = -1
         self.values = np.empty(self.capacity, dtype=int)
 
+    # O(n)
     def printed(self):
         if self.last_position == -1:
             print("The array is empty")
         else:
             for i in range(self.last_position + 1):
-                print(i, ' - ', self.values[i])
+                print(i, " - ", self.values[i])
+
+    def insert(self, value):
+        if self.last_position == self.capacity - 1:
+            print("Maximum capacity reached")
+        else:
+            self.last_position += 1
+            self.values[self.last_position] = value
+
 
 array = ArrayNotOrditaned(5)
 array.printed()
