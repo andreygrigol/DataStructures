bc3ff4450445d6427ed5655d7c03011b84548e58
AndreyRibeiro <andrey.grigoletto2@gmail.com>
2025-05-23 22:59:48 -0300

    Added the Ordered Arrays data structure

---
+++ ArrayOrdered.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+class OrderedArray:
+
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.last_position = -1
+        self.values = np.empty(self.capacity, dtype=int)
+
+    def printed(self):
+        if self.last_position == -1:
+            print("The array is empty")
+        else:
+            for i in range(self.last_position + 1):
+                print(i, " - ", self.values[i])
