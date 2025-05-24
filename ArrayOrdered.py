bc2d184434be2cf4717a73ff514afe8d4ba23e23
AndreyRibeiro <andrey.grigoletto2@gmail.com>
2025-05-23 23:08:56 -0300

    Class can now print each value in its own index

--- ArrayOrdered.py
+++ ArrayOrdered.py
@@ -14,3 +14,29 @@ class OrderedArray:
         else:
             for i in range(self.last_position + 1):
                 print(i, " - ", self.values[i])
+
+    def insert(self, value):
+        if self.last_position == self.capacity - 1:
+            print("Maximum capacity reached")
+            return
+
+        position = 0
+        for i in range(self.last_position + 1):
+            position = i
+            if self.values[i] > value:
+                break
+
+        x = self.last_position
+        while x >= position:
+            self.values[x + 1] = self.values[x]
+            x -= 1
+
+        self.values[position] = value
+        self.last_position += 1
+
+
+array = OrderedArray(10)
+array.printed()
+
+array.insert(6)
+array.printed()
